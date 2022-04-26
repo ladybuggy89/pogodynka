08d2dc9c43dce4c53c4cbcf508cc305898c16235
ladybuggy89 <martamaria.pater@gmail.com>
2022-04-26 19:20:52 +0200

    pogoda

---
+++ src/components/WeatherView.vue
@@ -0,0 +1,64 @@
+<template>
+<div class="weatherView">
+   <h1>Weather</h1> 
+   {{weather}}
+
+   <table>
+       <tr>
+           <th>City</th>
+           <td>{{cityName}}</td>
+       </tr>
+        <tr>
+           <th>LAT</th>
+           <td>{{lat}}</td>
+           </tr>
+            <tr>
+           <th>LON</th>
+           <td>{{lon}}</td>
+       </tr>
+   </table>
+   </div>
+</template>
+
+<script>
+
+
+export default {
+    name: 'WeatherView',
+    data(){
+        return{
+            cityName: "",
+            lat: "",
+            lon: ""
+
+        }
+    },
+    methods:{
+        getWeather(){
+            console.log("getWeather")
+        },
+        getLocation(cityName){
+            console.log(cityName)
+            fetch("http://api.openweathermap.org/geo/1.0/direct?q="+cityName+"&appid=04d03c358e8933ac6823da54c340c97b")
+            .then(dt => dt.json())
+            .then (dt => {
+                this.cityName = dt[0].name;
+                this.cityName = dt[0].lat;
+                this.cityName = dt[0].lon;
+
+
+               // console.log(dt[0])
+            })
+
+        },
+    },
+    created(){
+        this.getLocation("Katowice");
+    },
+    
+
+}
+</script>
+<style>
+
+</style>
